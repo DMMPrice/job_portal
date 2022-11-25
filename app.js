83c16e701fc5fce99859fbff623d4a18fa0660d8
Aniruddha Ghosh <ghoshaniruddha2003@gmail.com>
2022-11-25 22:02:48 +0530

    Basic Routing done

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
-const http= require('http');
-const server= http.createServer();
-server.listen(3000);
+const express = require('express');
+const path = require("path");
+const app = express();
+app.use(express.static('public'));
+
+//Server routing
+app.get('/', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'Views', 'index.html');
+    res.sendFile(htmlFilePath);
+})
+app.get('/login', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'Views', 'login.html');
+    res.sendFile(htmlFilePath);
+})
+app.get('/jobs', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'Views', 'Jobs.html');
+    res.sendFile(htmlFilePath);
+})
+app.get('/ad', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'Views', 'admin_page.html');
+    res.sendFile(htmlFilePath);
+})
+app.get('/contact', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'Views', 'Contact.html');
+    res.sendFile(htmlFilePath);
+})
+app.get('/register', function (req, res) {
+    const htmlFilePath = path.join(__dirname, 'Views', 'register.html');
+    res.sendFile(htmlFilePath);
+})
+
+//Port Number
+port = 3000;
+//Listen to the port number
+app.listen(port);
+//Sever link
+console.log(`Server is running at http://localhost:${port}/`);
